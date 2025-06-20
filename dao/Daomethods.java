faee34b0fb843723b983032fbc9092466c53831b
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-06-20 17:14:51 +0530

    Update Daomethods.java

--- dao/Daomethods.java
+++ dao/Daomethods.java
@@ -3,4 +3,8 @@ package conn.dao;
 public interface Daomethods {
 boolean createuser(String name,String email,String phonenumber,String dateofbirth,String password);
 boolean validuser(String email,String password);
+boolean forgotpassword(String email,String password);
+boolean validemail(String email);
+String getUsernameByEmail(String email);
+boolean updatepassword(String email,String password);
 }

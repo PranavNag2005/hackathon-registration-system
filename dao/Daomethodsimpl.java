6b1e27aa0cccb3f6d317a5bdd70da227f3cd6b70
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-06-20 17:16:31 +0530

    Update Daomethodsimpl.java

--- dao/Daomethodsimpl.java
+++ dao/Daomethodsimpl.java
@@ -44,4 +44,57 @@ public boolean validuser(String email,String password) {
 		}return false;
 		
 	}
+public boolean forgotpassword(String email,String password) {
+	
+	String query="update users set password=? where email=?";
+	try(Connection conn=Dbconnection.getconnection();){
+		PreparedStatement pt=conn.prepareStatement(query);
+		pt.setString(1,password);
+		pt.setString(2, email);
+		int rs=pt.executeUpdate();
+		return true;
+	} catch (SQLException e) {
+		
+		e.printStackTrace();
+	}return false;
+}
+public boolean validemail(String email) {
+	String query="select * from users where email=?";
+	try(Connection conn=Dbconnection.getconnection()){
+		PreparedStatement pt=conn.prepareStatement(query);
+		pt.setString(1, email);
+		ResultSet rs=pt.executeQuery();
+		return rs.next();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}return false;
+}
+public String getUsernameByEmail(String email) {
+    String name = null;
+    try (Connection con = Dbconnection.getconnection();
+         PreparedStatement ps = con.prepareStatement("SELECT name FROM users WHERE email = ?")) {
+        ps.setString(1, email);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            name = rs.getString("name");
+        }
+    } catch (Exception e) {
+        e.printStackTrace(); 
+    }
+    return name;
+}
+public boolean updatepassword(String email,String password)  {
+	try(Connection conn=Dbconnection.getconnection()){
+		PreparedStatement pt=conn.prepareStatement("update users set password=? where email=?");
+		pt.setString(1, password);
+		pt.setString(2, email);
+		int rs=pt.executeUpdate();
+		return true;
+	}
+	catch(Exception e) {
+		e.printStackTrace();
+	}
+	return false;
+}
 }

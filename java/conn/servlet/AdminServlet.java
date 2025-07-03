aebdc7966392b8371824413c9c6175e0765d7b69
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-07-03 17:26:06 +0530

    added admin servelt

---
+++ java/conn/servlet/AdminServlet.java
@@ -0,0 +1,56 @@
+package conn.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import conn.dao.Admindaoimpl;
+import conn.dao.CaptchaVerifier;
+
+
+@WebServlet("/AdminServlet")
+public class AdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     Admindaoimpl dao=new Admindaoimpl();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email=request.getParameter("email");
+		String password=request.getParameter("password");
+		String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
+		boolean isCaptchaValid = CaptchaVerifier.verifyCaptcha(gRecaptchaResponse);
+		if (!isCaptchaValid) {
+	        response.sendRedirect("adminlogin.jsp?error=Captcha failed"); // Notify user of CAPTCHA failure
+	        return; // Stop further execution
+	    }
+		if(isCaptchaValid) {
+			if(dao.validadmin(email, password)) {
+				HttpSession session=request.getSession();
+				 String name = dao.getAdminnameByEmail(email);
+				 int sid=dao.getadminidbyemail(email);
+				 session.setAttribute("admin_id", sid);
+				session.setAttribute("email", email);
+				session.setAttribute("name", name);
+				
+				response.sendRedirect("admindashboard.jsp");
+			}
+			else {
+				response.sendRedirect("adminlogin.jsp?invalidpassword=0");
+			}
+		}
+	}
+
+}

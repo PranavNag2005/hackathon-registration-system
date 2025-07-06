35649cc3ef4e2251405830b5458bae0eae2655a7
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-07-06 21:53:16 +0530

    added project file

---
+++ dao/Project.java
@@ -0,0 +1,65 @@
+package conn.dao;
+
+
+
+public class Project {
+    private String title;
+    private String domain;
+    private String description;
+    private String techStack;
+    private String githubUrl;
+   private boolean docsSubmitted;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTechStack() {
+        return techStack;
+    }
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
+    private boolean githubSubmitted;
+
+    public boolean isGithubSubmitted() {
+        return githubSubmitted;
+    }
+
+    public void setGithubSubmitted(boolean githubSubmitted) {
+        this.githubSubmitted = githubSubmitted;
+    }
+    public String getGithubUrl() {
+        return githubUrl;
+    }
+
+    public void setGithubUrl(String githubUrl) {
+        this.githubUrl = githubUrl;
+    }
+   
+    public void setDocsSubmitted(boolean docsSubmitted) { 
+        this.docsSubmitted = docsSubmitted; 
+    }
+    
+    public boolean isDocsSubmitted() {
+    	return docsSubmitted;
+    }
+    
+}

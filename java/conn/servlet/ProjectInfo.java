734a514d222c8fabd81a9387b36f50144646556a
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-07-10 17:46:39 +0530

    project information

---
+++ java/conn/servlet/ProjectInfo.java
@@ -0,0 +1,70 @@
+package conn.servlet;
+
+
+public class ProjectInfo {
+    private String name;
+    private String email;
+    private int pid;
+    private String projectTitle;
+    private String projectDomain;
+    private String projectDescription;
+    private String technologyStack;
+    private String githubUrl;
+    private String filepath;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getProjectTitle() {
+		return projectTitle;
+	}
+	public void setProjectTitle(String projectTitle) {
+		this.projectTitle = projectTitle;
+	}
+	public String getProjectDomain() {
+		return projectDomain;
+	}
+	public void setProjectDomain(String projectDomain) {
+		this.projectDomain = projectDomain;
+	}
+	public String getProjectDescription() {
+		return projectDescription;
+	}
+	public void setProjectDescription(String projectDescription) {
+		this.projectDescription = projectDescription;
+	}
+	public String getTechnologyStack() {
+		return technologyStack;
+	}
+	public void setTechnologyStack(String technologyStack) {
+		this.technologyStack = technologyStack;
+	}
+	public String getGithubUrl() {
+		return githubUrl;
+	}
+	public void setGithubUrl(String githubUrl) {
+		this.githubUrl = githubUrl;
+	}
+	public String getFilepath() {
+		return filepath;
+	}
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+
+    // Getters and Setters (Auto-generate via IDE or Lombok)
+}

774ae41c14b37c6e2d9c3e9bbbb6550cb96ae0c4
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-07-11 21:52:37 +0530

    Update Users.java

--- java/conn/servlet/Users.java
+++ java/conn/servlet/Users.java
@@ -1,7 +1,57 @@
 package conn.servlet;
 
 import java.sql.Date;
+package conn.servlet;
 
+import java.sql.Date;
+
+public class Users {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private Date dateOfBirth;
+    private String rollNumber;
+    private String branch;
+    private int yearOfStudy;
+    private String collegeName;
+
+
+    // Getters
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public Date getDateOfBirth() { return dateOfBirth; }
+
+    // Setters
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public void setDateOfBirth(Date dateOfBirth) { this.dateOfBirth = dateOfBirth; }
+	public String getRollNumber() {
+		return rollNumber;
+	}
+	public void setRollNumber(String rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public int getYearOfStudy() {
+		return yearOfStudy;
+	}
+	public void setYearOfStudy(int yearOfStudy) {
+		this.yearOfStudy = yearOfStudy;
+	}
+	public String getCollegeName() {
+		return collegeName;
+	}
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+}
 public class Users {
     private String name;
     private String email;

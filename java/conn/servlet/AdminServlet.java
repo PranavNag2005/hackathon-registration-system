18fdd323eaaa6dc475e5c5024f7aa4256faae810
Pranav Nag <153430540+PranavNag2005@users.noreply.github.com>
2025-07-12 21:27:20 +0530

    Update AdminServlet.java

--- java/conn/servlet/AdminServlet.java
+++ java/conn/servlet/AdminServlet.java
@@ -1,5 +1,6 @@
-package conn.servlet;
+	package conn.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,15 +9,19 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import conn.dao.Admindaoimpl;
 import conn.dao.CaptchaVerifier;
-
-
+import conn.dao.Dbconnection;
+import java.sql.*;
+import java.util.*;
 @WebServlet("/AdminServlet")
 public class AdminServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
      Admindaoimpl dao=new Admindaoimpl();
+     
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -25,7 +30,9 @@ public class AdminServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	
+	protected void doGet(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException{
+		
+	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String email=request.getParameter("email");
@@ -39,7 +46,8 @@ public class AdminServlet extends HttpServlet {
 		if(isCaptchaValid) {
 			if(dao.validadmin(email, password)) {
 				HttpSession session=request.getSession();
-				 String name = dao.getAdminnameByEmail(email);
+			 	 String name = dao.getAdminnameByEmail(email);
+			 	
 				 int sid=dao.getadminidbyemail(email);
 				 session.setAttribute("admin_id", sid);
 				session.setAttribute("email", email);
@@ -54,6 +62,55 @@ public class AdminServlet extends HttpServlet {
 				session.setAttribute("bothCount", both);
 				session.setAttribute("registeredCount", registered);
 				session.setAttribute("projectsubmitted",project);
+				String query="SELECT project_domain, COUNT(*) AS total_count FROM project GROUP BY project_domain;";
+				try(Connection connn=Dbconnection.getconnection()){
+					
+					Statement st=connn.createStatement();
+					ResultSet rs=st.executeQuery(query);
+					while (rs.next()) {
+		                String domain = rs.getString("project_domain");
+		                int count = rs.getInt("total_count");
+
+		                switch (domain) {
+		                    case "Web Development":
+		                    	System.out.println("WEb"+count);
+		                        session.setAttribute("webDevCount", count);
+		                        break;
+		                    case "AI & ML":
+		                    	session.setAttribute("aiCount", count);
+		                    	System.out.println("ai"+count);
+		                        break;
+		                    case "CloudComputing":
+		                    	session.setAttribute("cloudCount", count);
+		                    	System.out.println("clo"+count);
+		                        break;
+		                    case "IOT Solutions":
+		                    	session.setAttribute("iotCount", count);
+		                        break;
+		                    case "Game Dev":
+		                    	session.setAttribute("gameDevCount", count);
+		                        break;
+		                    case "Mobile App Dev":
+		                    	session.setAttribute("mobileCount", count);
+		                        break;
+		                    case "Sensor-Based Applications":
+		                    	session.setAttribute("sensorCount", count);
+		                        break;
+		                    case "Data Science":
+		                    	session.setAttribute("dataScienceCount", count);
+		                        break;
+		                    default:
+		                        break;
+		                }
+		                Map<String, Integer> chartData = dao.getRatingStatusByAdmin(name);
+		                session.setAttribute("rated", chartData.get("rated"));
+		        		session.setAttribute("pending", chartData.get("pending"));
+		            }
+					
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
 				response.sendRedirect("admindashboard.jsp");
 			}
 			else {
